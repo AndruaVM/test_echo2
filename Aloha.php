30e26fbdd997fb1418986e0864c97adad72a7f69
Parvus <muzyka@beargg.com>
2019-07-03 20:47:39 +0300

    initial commit

---
+++ Aloha.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 03.07.2019
+ * Time: 19:51
+ */
+class Aloha
+{
+    public function getOne(){
+
+        return 1;
+    }
+}
+echo (new Aloha())->getOne();
